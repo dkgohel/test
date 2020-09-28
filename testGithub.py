d08c1b83a4f440ea874ad486e0e7cb8252bcda0c
dkgohel <darshan.gohel619@gmail.com>
2020-09-28 12:52:48 +0300

    revert garbage adding

--- testGithub.py
+++ testGithub.py
@@ -3,7 +3,5 @@ print("This is my first Github Clone, Add, Staging Area, Push and Commit")
 n=int(input("Size of Array ?"))
 if(n>10):
     print("Too big")
-    print("n^10 darshan")
-    print("Nice work")
 else:
     print("Average Array")
