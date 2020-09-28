b8ca8d0685c7c1a9512aa0df37eb154f63dc6212
dkgohel <darshan.gohel619@gmail.com>
2020-09-28 12:36:44 +0300

    Added Prompt

--- testGithub.py
+++ testGithub.py
@@ -1,2 +1,7 @@
 print("Hello world")
 print("This is my first Github Clone, Add, Staging Area, Push and Commit")
+n=int(input("Size of Array ?"))
+if(n>10):
+    print("Too big")
+else:
+    print("Average Array")
