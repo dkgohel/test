ebc127edd7aa9d2872cdd31b2186310cbff78759
dkgohel <darshan.gohel619@gmail.com>
2020-09-28 12:55:53 +0300

    Adding some garbage

--- testGithub.py
+++ testGithub.py
@@ -3,5 +3,7 @@ print("This is my first Github Clone, Add, Staging Area, Push and Commit")
 n=int(input("Size of Array ?"))
 if(n>10):
     print("Too big")
+    print("2nd time commit")
+    print("Adding some garbages")
 else:
     print("Average Array")
