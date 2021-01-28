f2099302c725486729be312e74b4d7d001ff67b6
Darshan Gohel <45890158+dkgohel@users.noreply.github.com>
2021-01-28 11:11:22 +0300

    update file

--- testGithub.py
+++ testGithub.py
@@ -1,14 +1,15 @@
 print("Hello world")
 print("This is my first Github Clone, Add, Staging Area, Push and Commit")
+print("Added from admin")
 n=int(input("Size of Array ?"))
-if(n>10):
+if(n>11):
     print("Too big")
     print("2nd time commit")
     print("Adding some garbages")
     print("Add this for merge conflict")
 else:
     s=input("Are you fan of virat kohli?")
-    if s=="yes":
+    if s=="Yes":
         print("We are in the same zone")
     else:
         print("We are enemy")
